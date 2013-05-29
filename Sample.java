6aee970765e4817fcc3464dc5a897bc835f5185f
Ramu <boys21century@gmail.com>
2013-05-29 17:30:33 +0530

    Update Sample.java
    added a new function to the sample Java file.

--- Sample.java
+++ Sample.java
@@ -1,3 +1,6 @@
+
+
+
 class Sample {
 
 
@@ -5,6 +8,12 @@ public static void main(String[] args){
 
 System.out.println("fisrt java progranm");
 
+void fun(){
+  
+  
+  System.out.println("added this fundtion in git hub repository!");
+  
+}
 
 
 }
